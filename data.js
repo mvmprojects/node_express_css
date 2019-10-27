f902e2358c6aa3ddce04936445de4faef572ef5a
nestedforloop <nestedforloop@users.noreply.github.com>
2019-10-27 22:39:25 +0100

    commit assignment to new branch

---
+++ data.js
@@ -0,0 +1,5 @@
+module.exports = [
+    { "id": "1", "name": "customer1", "email": "customer1@hr.nl", "password": "password"},
+    { "id": "2", "name": "customer2", "email": "customer2@hr.nl", "password": "password"},
+    { "id": "3", "name": "customer3", "email": "customer3@hr.nl", "password": "password"},
+];
